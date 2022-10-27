9e17ec46147aba896549be8a2702b0224c608c04
Meriem-bounaas <bouunaas.meriem.1993@gmail.com>
2022-10-27 17:30:03 +0200

    create ui

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors:{
+      firstColor:'#3d3e42',
+      secondColor:'#31333b',
+      thirdColor: '#ffc86b',
+      forthColor: '#f9f9f9',
+      fifthColor:'#b6b7b9',
+      sixthColor: '#58595b',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
