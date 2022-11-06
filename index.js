05b4792c562dbfb176dc2a95c94dcfb00061536b
Meriem-bounaas <bouunaas.meriem.1993@gmail.com>
2022-11-06 21:15:00 +0100

    made it responsive and add section of experience

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ for (let i = 0; i < menuItems.length; i++) {
     
 }
 
-const myDetail = document.getElementById('my-detail')
-const myExperience =  document.getElementById('my-cercle').addEventListener('click', ()=>{
-    if (myDetail.style.display !== "none") {
-        myDetail.style.display = "none";
-    } else {
-        myDetail.style.display = "block";
-    }
-})
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    colors:{
-      firstColor:'#3d3e42',
-      secondColor:'#31333b',
-      thirdColor: '#ffc86b',
-      forthColor: '#f9f9f9',
-      fifthColor:'#b6b7b9',
-      sixthColor: '#58595b',
+    extend: {
+      colors:{
+        firstColor:'#3d3e42',
+        secondColor:'#31333b',
+        thirdColor: '#ffc86b',
+        forthColor: '#f9f9f9',
+        fifthColor:'#b6b7b9',
+        sixthColor: '#58595b',
+      },
+      fontFamily:{
+        'title-font': ['Source Serif Pro'],
+        'font-text': ['Hind']
+      },
+      keyframes: {
+        topbottom: {
+          '0%, 100%': { transform:' translateY(0)' },
+          '50%': { transform: 'translateY(10px)' },
+        }
+      },
+      animation: {
+        topbottom: 'topbottom 0.9s ease-in-out infinite',
+      }
     },
-    fontFamily:{
-      'title-font': ['Source Serif Pro'],
-      'font-text': ['Hind']
-    },
-    extend: {},
   },
   plugins: [],
 }
